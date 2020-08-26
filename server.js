e7d57adf062a58913c339f84f1021d7309f5d06f
Ajuogaaz <brianlinus1753@gmail.com>
2020-08-26 01:07:03 -0400

    Concluded

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 
 const express = require("express");
-
-const app = express();
-
+const bodyParser = require("body-parser");
 const port = 3000;
+const app = express();
+//We can use options like json
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", (req, res) => {
-    res.send("Thanks for sending");
+   let num1 = Number(req.body.num1);
+   let num2 = Number(req.body.num2);
+
+   let result = num1 + num2;
+
+   res.send("<h2>result is: " + result + "</h2>");
+
 });
 
 app.listen(port, () =>{
